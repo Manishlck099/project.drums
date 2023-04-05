46948ed6d28340004bdd2e1026e4b12509c65122
Manishlck099 <123995022+Manishlck099@users.noreply.github.com>
2023-04-05 18:31:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,37 +11,37 @@ for (let i = 0; i < button.length; i++) {
 
         switch (buttonInnerHTML) {
             case "w":
-                let audio1 = new Audio("sounds/crash.mp3");
+                 let audio1 = new Audio("kick-bass.mp3");
                 audio1.play();
                 break;
-
+                
             case "a":
-                let audio2 = new Audio("sounds/kick-bass.mp3");
+                let audio2 = new Audio("kick-bass.mp3");
                 audio2.play();
                 break;
 
             case "s":
-                let audio3 = new Audio("sounds/snare.mp3");
+                let audio3 = new Audio("snare.mp3");
                 audio3.play();
                 break;
 
             case "d":
-                let audio4 = new Audio("sounds/tom-1.mp3");
+                let audio4 = new Audio("tom-1.mp3");
                 audio4.play();
                 break;
 
             case "j":
-                let audio5 = new Audio("sounds/tom-2.mp3");
+                let audio5 = new Audio("tom-2.mp3");
                 audio5.play();
                 break;
 
             case "k":
-                let audio6 = new Audio("sounds/tom-3.mp3");
+                let audio6 = new Audio("tom-3.mp3");
                 audio6.play();
                 break;
 
             case "l":
-                let audio7 = new Audio("sounds/tom-4.mp3");
+                let audio7 = new Audio("tom-4.mp3");
                 audio7.play();
                 break;
             default: console.log(buttonInnerHTML)
